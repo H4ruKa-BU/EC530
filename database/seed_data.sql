f756fc1dd1c60c3ab3c830342f636f868887ff7a
H4ruKa-BU <baichuan.xu6@gmail.com>
2025-04-25 05:02:46 -0400

    Create seed_data.sql

---
+++ database/seed_data.sql
@@ -0,0 +1,4 @@
+-- Insert example submissions
+INSERT INTO submissions (student_id, submission_text, score, feedback)
+VALUES
+('u123456', 'Sample essay text...', 85, 'Well-written analysis with clear structure.');
